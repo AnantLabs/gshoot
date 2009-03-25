386317b4e4591f3ae94f89c00fd311e0bcee1f3d
vinci.zhang <vinci.zhang@8a956bba-163c-11de-9433-ad2e3fad96c5>
2009-03-25 14:32:39 +0000

    [zxq] - refactory document to parser: part 4

---
+++ test/unit/com/vinci/gshoot/fileparser/HtmlParserTest.java
@@ -0,0 +1,15 @@
+package com.vinci.gshoot.fileparser;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+import java.io.File;
+
+public class HtmlParserTest {
+    @Test
+    public void should_parse_content_of_html() throws Exception {
+        File f = new File("fixtures/html/jquery.html");
+        String content = new HtmlParser().parse(f).getTextContent();
+//        assertTrue(content.contains("教程"));
+    }
+}
